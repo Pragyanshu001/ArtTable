c14c21b24ebdc572b5ab83fbd87b5f8e666ce604
Pragyanshu Gupta <pragyanshugupta001@gmail.com>
2025-11-01 22:26:58 +0530

    react

--- src/components/ArtTable.tsx
+++ src/components/ArtTable.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from "react";
+import { useEffect, useState, useRef } from "react";
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
 import { fetchArtworks } from "../api/artworkApi";
